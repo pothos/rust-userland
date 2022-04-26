62b8bf86e9d88079410342bddaa84d78bdcc2e9b
Kai Lüke <kailueke@riseup.net>
2022-04-27 00:37:31 +0900

    Use build script to simplify Dockerfile
    The repetition made the file hard to read which hinders editing.
    Use a helper script that does the common tasks.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+PROJECT="$1"
+shift
+BIN="${BIN-}"
+PATCH="${PATCH-}"
+
+if [ "$BIN" = "" ]; then
+  BIN=$(echo "$PROJECT" | cut -d / -f 5)
+fi
+
+git clone "$PROJECT" "$BIN"
+cd "$BIN"
+if [ "$PATCH" != "" ]; then
+  git apply "$PATCH"
+fi
+source "$HOME/.cargo/env"
+RUSTFLAGS="-C target-feature=+crt-static" mold -run cargo build --target x86_64-unknown-linux-musl --release "$@"
+mkdir -p /rust-bin
+strip target/x86_64-unknown-linux-musl/release/"$BIN"
+mv target/x86_64-unknown-linux-musl/release/"$BIN" /rust-bin/
